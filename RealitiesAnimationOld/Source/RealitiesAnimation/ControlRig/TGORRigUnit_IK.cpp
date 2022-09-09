c304b9af51784d4d3beb501f5d9a42e57a4c1c46
Angry Lizard <gitlab@netliz.net>
2022-09-09 18:53:36 +0200

    Moved animation, procedural and rendering into their own submodules

--- RealitiesAnimationOld/Source/RealitiesAnimation/ControlRig/TGORRigUnit_IK.cpp
+++
@@ -1,7 +0,0 @@
-// The Gateway of Realities: Planes of Existence.
-
-#include "TGORRigUnit_IK.h"
-
-#include "RealitiesUtility/Structures/TGOR_Matrix3x3.h"
-#include "RealitiesUtility/Utility/TGOR_Math.h"
-#include "Units/RigUnitContext.h"
--- RealitiesRenderingOld/Source/RealitiesRendering/Actors/TGOR_SkyboxActor.cpp
+++
@@ -1,9 +0,0 @@
-// The Gateway of Realities: Planes of Existence.
-
-
-#include "TGOR_SkyboxActor.h"
-
-ATGOR_SkyboxActor::ATGOR_SkyboxActor()
-	: Super()
-{
-}
